48921a14d3acef67f612efb1e7066a89e4bf102e
Nitesh Jaswal <nitesh1511@gmail.com>
2023-07-15 16:36:36 +0000

    Added `down` command

--- app/invoke.sh
+++ app/invoke.sh
@@ -6,25 +6,21 @@ export PYTHONPATH="$(pwd)"
 # Reference: https://opensource.com/article/19/12/help-bash-program
 Help()
 {
-   echo "Helps interact with the app build system."
-   echo
-   echo "Syntax: sh invoke.sh [help|run|build|redeploy|test_unit|test_integration]"
-   echo "options:"
-   echo "help               Print this help."
-   echo "run                Brings up the docker container from latest image."
-   echo "build              Builds the docker image."
-   echo "redeploy           Rebuilds and redeploys the docker container."
-   echo "test_unit          Run unit tests."
-   echo "test_integration   Run integration tests."
-   echo
+    echo "Helps interact with the app build system."
+    echo
+    echo "Syntax: sh invoke.sh [help|run|build|redeploy|down|test_unit|test_integration]"
+    echo "options:"
+    echo "help               Print this help."
+    echo "run                Brings up the docker container from latest image."
+    echo "build              Builds the docker image."
+    echo "redeploy           Rebuilds and redeploys the docker container."
+    echo "down               Stops the docker container."
+    echo "test_unit          Run unit tests."
+    echo "test_integration   Run integration tests."
+    echo
 }
 
 case ${1:-} in
-    "build")
-        echo "Building..."
-        docker build -t onigiri-app:latest .
-        ;;
-    
     "test_unit")
         echo "Running unit tests..."
         poetry run pytest tests/unit
@@ -33,20 +29,30 @@ case ${1:-} in
     "test_integration")
         echo "Running integration tests..."
         poetry run pytest tests/integration
-        echo "If tests fail, make sure the app is running with 'sh invoke.sh run'"
+        echo "If tests fail to run, make sure the app is running with 'sh invoke.sh run'"
+        ;;
+
+    "build")
+        echo "Building..."
+        docker build -t onigiri-app:latest .
         ;;
 
     "run")
         echo "Running docker container..."
-        docker run --rm --name onigiri_app -p 8000:8000 onigiri-app:latest
+        docker run -d --rm --name onigiri_app -p 8000:8000 onigiri-app:latest
         ;;
     
     "redeploy")
         echo "Rebuilds and redeploys container..."
         docker build -t onigiri-app:latest .
-        docker run --rm --name onigiri_app -p 8000:8000 onigiri-app:latest
+        docker run -d --rm --name onigiri_app -p 8000:8000 onigiri-app:latest
         ;;
     
+    "down")
+        echo "Stopping container..."
+        docker stop onigiri_app
+        ;;
+
     "help")
         Help
         ;;
@@ -57,3 +63,4 @@ case ${1:-} in
         exit 1
         ;;
 esac
+    
